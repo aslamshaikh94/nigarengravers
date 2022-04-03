15ad08660502afa250b6d40a90e5da0fe18c3db6
Aslam Ahmed <highflyingaslam@gmail.com>
2022-04-03 17:29:48 +0530

    changes

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-
-app.listen(9000)
+// Server setup
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log('Server is running ' + PORT)
+})
