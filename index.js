c4a0cc9839912f8bcdd8204675213bf50d9b9bd6
Aslam Ahmed <highflyingaslam@gmail.com>
2022-05-27 19:44:41 +0530

    restore ga

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, '/build')))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/build', 'index.html'))
 })
+
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/build', 'index.html'))
 })
+
 // Server setup
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
